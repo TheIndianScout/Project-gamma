f566d1576dbcdbabef06d4ac905972cb88457911
TheIndianScout <theindianscout@proton.me>
2025-01-07 01:19:49 +0530

    Made Contact page and Optimized the site

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import PlayerProfile from './components/PlayerProfile';
-import Login from './components/Login';
-import ClubProfile from './components/ClubProfile';
-// import PlayerView from './components/PlayerView';
-// import ClubView from './components/ClubView';
-import Footer from './components/Footer';
-import './index.css';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './index.css';
+import Loader from 'react-loaders'
+const Navbar = lazy(() => import('./components/Navbar.jsx'));
+const Home = lazy(() => import('./components/Home.jsx'));
+const About = lazy(() => import('./components/About.jsx'));
+const Contact = lazy(() => import('./components/Contact.jsx'));
+const PlayerProfile = lazy(() => import('./components/PlayerProfile.jsx'));
+const ClubProfile = lazy(() => import('./components/ClubProfile.jsx'));
+const Login = lazy(() => import('./components/Login.jsx'));
+const Footer = lazy(() => import('./components/Footer.jsx'));
 
 function App() {
 
   return (
     <div className="App" style={{ scrollBehavior: 'smooth' }}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} >/</Route>
-          <Route path='/about' element={<About />} >/</Route>
-          <Route path='/contact' element={<Contact />} >/</Route>
-          <Route path='/login' element={<Login />} >/</Route>
-          <Route path='/player-profile' element={<PlayerProfile />} >/</Route>
-          <Route path='/club-profile' element={<ClubProfile />} >/</Route>
-        </Routes>
-      </Router>
+      <Suspense fallback={<Loader/>}>
+        <Router>
+          <Navbar />
+          <Routes>
+              <Route path='/' element={<Home />} >/</Route>
+            <Route path='/about' element={<About />} >/</Route>
+            <Route path='/contact' element={<Contact />} >/</Route>
+            <Route path='/login' element={<Login />} >/</Route>
+            <Route path='/player-profile' element={<PlayerProfile />} >/</Route>
+            <Route path='/club-profile' element={<ClubProfile />} >/</Route>
+          </Routes>
+        </Router>
+      </Suspense>
       <Footer />
     </div>
   );
